27a0eeb462f69e1071491c549b5abaf6a7b537ad
Avril Cui <90942185+Avril-Cui@users.noreply.github.com>
2023-09-18 19:03:20 -0400

    fix: layout and labels in dashboard page, new columns to portfolio table (#10)
    * fix: saving process
    
    * fix: issue with macro parameters function
    
    * fix: issue between macro parameters and combinated price
    
    * fix: random seed issue that caused distort in price
    
    * fix: current price, day chart, graphs fixed
    
    * feat: no close market new price fetch updated
    
    * fix: layout and labels in dashboard page, new columns to portfolio table

--- server/Function/helper.py
+++ server/Function/helper.py
@@ -5,5 +5,5 @@ class helper:
         index_tmp = int(current_time-start_time)
         current_prices = {}
         for company in company_list:
-            current_prices[company] = round(float(flat_list[company][index_tmp]))
+          current_prices[company] = round(float(flat_list[company][index_tmp]), 2)
         return current_prices
